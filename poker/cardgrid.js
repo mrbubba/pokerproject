f897eaeac9a584ef3838c08deb5bd1baa909b69d
MrBubba <bubbashipwreck@gmail.com>
2014-07-14 10:53:50 -0700

    finished the card grid hand ranges now saved in a javascript variable called profile

--- poker/cardgrid.js
+++ poker/cardgrid.js
@@ -2,4 +2,16 @@ $(document).ready(function()  {
 	$("td").click(function() {
 		$(this).toggleClass("select");
 	});
+	
+	var profile = ""
+	
+	$("#save").click(function() {
+		profile = "";
+		$("td").each(function() {
+			if ($(this).hasClass("select"))
+			{profile = profile + $(this).attr("id")+","};
+		});
+		$("ul").text(profile);
+	});
 });
+	
